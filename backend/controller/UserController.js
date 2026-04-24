f8ba5d9f0f25272c3d66c8a4a4c30755a30faa41
Khacsy05 <khacsy0@gmail.com>
2026-04-25 02:11:04 +0700

    update

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -17,22 +17,35 @@ export const friendRequest = async (req,res) => {
             })
         }
 
-        const existed = await FriendRequest.findOne({
-            from,
-            to,
-            status: "pending"
-        });
-
-        if (existed) {
-            return res.status(400).json({
-                message: "Đã gửi lời mời trước đó"
-            });
-        }
-
-        if (from === to) {
+        if (from.toString() === to.toString()) {
             return res.status(400).json({
                 message: "Không thể kết bạn với chính mình"
             });
+        }
+
+        const existed = await FriendRequest.findOne({
+            $or: [
+                { from, to },
+                { from: to, to: from }
+            ]
+        });
+
+
+        if (existed) {
+            if(existed.status === "pending"){
+                return res.status(400).json({ message: "Đã gửi lời mời rồi" });
+            }
+            if (existed.status === "accepted") {
+                return res.status(400).json({ message: "Đã là bạn bè" });
+            }
+            if (existed.status === "rejected") {
+                existed.status = "pending";
+                existed.from = from;
+                existed.to = to;
+                await existed.save();
+            }
+
+            return res.json({ message: "Gửi lại lời mời thành công" });
         }
 
         const request = new FriendRequest({
@@ -47,3 +60,116 @@ export const friendRequest = async (req,res) => {
         res.status(500).json({message: "Loi he thong"})
     }
 }
+
+export const getAllFriend = async (req,res) => {
+    try {
+        const user = await User.findOne({ userId: req.user._id });
+        const relations = await FriendRequest.find({
+            status: "accepted",
+            $or: [{from: user._id} , {to: user._id}]
+        }).populate("from to","fullname")
+
+        if(!relations){
+            return res.status(400).json({
+                message : "Khong co sinh vien"
+            })
+        }
+
+        const friends = relations.map(r => {
+            return r.from._id.equals(user._id)
+            ? r.to
+            : r.from
+        })
+
+        res.status(200).json(friends)
+    } catch (error) {
+        console.error("Loi khi goi getAllFriend",error);
+        res.status(500).json({message: "Loi he thong"})
+    }
+}
+
+export const getFriendRequest = async(req,res) => {
+    try {
+        const user = await User.findOne({ userId: req.user._id });
+        const request = await FriendRequest.find({
+            to: user._id,
+            status: "pending"
+        }).populate("from","fullname email")
+        res.json(request);
+    } catch (error) {
+        console.error("Loi khi goi getAllFriend",error);
+        res.status(500).json({message: "Loi he thong"})
+    }
+}
+
+export const acceptFriend = async (req, res) => {
+    try {
+        const user = await User.findOne({ userId: req.user._id });
+        const {requestId} = req.body;
+        const request = await FriendRequest.findById(requestId);
+        if (!request) {
+            return res.status(404).json({ message: "Không tìm thấy request" });
+        }
+
+        if (request.to.toString() !== user._id.toString()) {
+            return res.status(403).json({ message: "Không có quyền" });
+        }
+        request.status = "accepted";
+        await request.save();
+        res.json({ message: "Đã chấp nhận kết bạn" });
+    } catch (error) {
+        console.error("Loi khi goi acceptFriend",error);
+        res.status(500).json({message: "Loi he thong"})
+    
+    }
+}
+
+export const rejectFriend = async (req, res) => {
+  try {
+    const user = await User.findOne({ userId: req.user._id });
+    const { requestId } = req.body;
+
+    const request = await FriendRequest.findById(requestId);
+
+    if (!request) {
+      return res.status(404).json({ message: "Không tìm thấy request" });
+    }
+
+    if (request.to.toString() !== user._id.toString()) {
+      return res.status(403).json({ message: "Không có quyền" });
+    }
+
+    request.status = "rejected";
+    await request.save();
+
+    res.json({ message: "Đã từ chối kết bạn" });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+export const cancelRequest = async (req, res) => {
+  try {
+    const user = await User.findOne({ userId: req.user._id });
+    const { requestId } = req.body;
+
+    const request = await FriendRequest.findById(requestId);
+
+    if (!request) {
+      return res.status(404).json({ message: "Không tìm thấy request" });
+    }
+
+    if (request.from.toString() !== user._id.toString()) {
+      return res.status(403).json({ message: "Không có quyền huỷ" });
+    }
+
+    await request.deleteOne();
+
+    res.json({ message: "Đã huỷ lời mời" });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
